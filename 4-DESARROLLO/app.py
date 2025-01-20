294680916229dff0f2f76a88d0c9ac53233f3c28
jfegasu <jfegasu@gmail.com>
2025-01-20 10:31:42 -0500

    .

--- 4-DESARROLLO/app.py
+++ 4-DESARROLLO/app.py
@@ -6,19 +6,12 @@ app=Flask(__name__)
 @app.route("/")
 def root():
     return "SERVICIO NACIONAL DE APRENDIZAJE ****"
-@app.route("/N1")
-def Nivel1():
+@app.route("/N")
+def Nivel():
     msgito="Hola Papa"
     kk1="Teleinormatica"
-    z1='''
-    @app.route("/N1")
-def Nivel1():
-    msgito="Hola Papa"
-    kk1="Teleinormatica"
-    return render_template("prueba.html",msg=msgito,k1=kk1)
-
-    '''
+    
     b=[1,4,5,6]
-    return render_template("prueba.html",N=2)
+    return render_template("prueba.html",N=3)
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
